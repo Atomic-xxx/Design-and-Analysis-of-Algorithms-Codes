49f3be4f27a000bc100ec4fd7c1437f245342375
Atomic-xxx <mayank.13646@stu.upes.ac.in>
2025-09-09 09:53:57 +0530

    Week1

--- Week1/Armstrong.c
+++ Week1/Armstrong.c
@@ -1,32 +1,88 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
+int main() {
     int num;
-    printf("Enter a number: ");
-    scanf("%d",&num);
     int count=0;
-    for(int i=num;i>0;)
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+    int copy=num;
+    int x=num;
+    int sum=0;
+    while(copy>0)
     {
-        i = i/10;
+        copy=copy/10;
         count++;
     }
-    printf("%d\n",count);
-    int sum=0;
-    for(int i=num;i>0;)
+    printf("Number of digits =%d\n",count);
+    while(x>0)
     {
-        int digit = i%10;
-    
-        sum = sum + pow(digit,count);
-        i = i/10;
+        int d=x%10;
+        sum = sum + (int)(pow(d, count) + 0.5);
+
+        x=x/10;
     }
+    printf("Sum =%d\n",sum);
+
     if(sum == num)
     {
-        printf("The number %d is armstrong",num);
+        printf("The entered number is an Armstrong Number\n");
     }
     else
     {
-        printf("The number %d is not armstrong",num);
+        printf("The entered number is not an Armstrong Number\n");
     }
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
